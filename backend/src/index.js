4f303c30721894e135b0d888dadaa70bada0e49f
Sarah-2008 <sarahvitoriaffranca4@gmail.com>
2026-04-01 14:50:06 -0300

    ip dando erro

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import "dotenv/config"
 import authRoutes from "./routes/authRoutes.js"
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000
