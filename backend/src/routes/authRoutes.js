4c97672c202b8d9af27039c215062b931ae0225b
Sarah-2008 <sarahvitoriaffranca4@gmail.com>
2026-04-06 15:15:11 -0300

    Verificar erros

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -18,6 +18,29 @@ router.get("/register", async (req, res) => {
         if(password.lenght < 3){
             return res.status(400).json({ message: "A SENHA DEVERÁ CONTER MAIS DE TRÊS CARACTERES" })
         }
+
+        // checar se existe
+        const existingEmail = await User.findOne({email})
+        if(existingEmail){
+            return res.status(400).json({ message : "Email já cadastrado!"}) 
+        }
+
+        const existingUsername = await User.findOne({username})
+        if(existingUsername){
+            return res.status(400).json({ message : "Nome já cadastrado!"}) 
+        }
+
+        // coletar avatar aleatório
+        const profileImage = `https://api.dicebear.com/7.x/avataaars/svg?seed=${username}`;
+
+        const user = new User({
+            email,
+            username,
+            password,
+            profileImage: "",
+        })
+
+        await user.save();
     }catch (error){
 
     }
