f05ce8188908908b6eeff4c7525629b43401eaf1
Sarah-2008 <sarahvitoriaffranca4@gmail.com>
2026-04-06 15:58:29 -0300

    Senha

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,5 +1,11 @@
 import express from "express"
 import User from "../models/User.js";
+import jwt from "jsonwebtoken"
+
+const generateToken = (userId) => {
+    // jwt.sign({userId})
+    jwt.sign({userId}, process.env.JWT_SECRET, { expiresIn : "15d" })
+}
 
 const router = express.Router();
 
@@ -41,6 +47,19 @@ router.get("/register", async (req, res) => {
         })
 
         await user.save();
+
+        const token = generateToken(user._id)
+
+        res.status(201).json({
+            token , 
+            user : { 
+                _id : user._id,
+                username : user.username,
+                email : user.email,
+                profileImage : user.profileImage,
+            }
+        })
+
     }catch (error){
 
     }
